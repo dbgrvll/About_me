c7d321489d0e9b01305abb594d5fe2fdad7df523
Jin Woo Kim <jinwoov@gmail.com>
2019-10-31 21:27:06 -0700

    added script on JS file. Still struggling, but hopeful

--- app.js
+++ app.js
@@ -1,70 +1,141 @@
 'use strict';
 
-var askName = prompt('What\'s your name?');
-alert('Welcome ' + askName + ', exciting info about "Jin" and Juice ahead!');
+// var askName = prompt('What\'s your name?');
+// alert('Welcome ' + askName + ', exciting info about "Jin" and Juice ahead!');
 
-var preferredName = prompt('Is my preferred name JinK?');
-preferredName = preferredName.toLowerCase();
+// var preferredName = prompt('Is my preferred name JinK?');
+// preferredName = preferredName.toLowerCase();
 
-// console.log(preferredName);
+// // console.log(preferredName);
 
-if (preferredName === 'yes' || preferredName === 'y') {
-  alert('Boo, Wrong!');
-} else {
-  alert('You da best! Correct');
-}
+// if (preferredName === 'yes' || preferredName === 'y') {
+//   alert('Boo, Wrong!');
+// } else {
+//   alert('You da best! Correct');
+// }
 
-var askUniversity = prompt('Did I graduate from University of Hawaii at Manoa?');
-askUniversity = askUniversity.toLowerCase();
+// var askUniversity = prompt('Did I graduate from University of Hawaii at Manoa?');
+// askUniversity = askUniversity.toLowerCase();
 
-// console.log(askUniversity);
+// // console.log(askUniversity);
 
-if (askUniversity === 'yes' || askUniversity === 'y') {
-  alert('Very nice! you been creeping on my info');
-  // console.log('Very nice! you been creeping on my info');
-} else {
-  alert('Try again!');
-  // console.log('Try again!');
-}
+// if (askUniversity === 'yes' || askUniversity === 'y') {
+//   alert('Very nice! you been creeping on my info');
+//   // console.log('Very nice! you been creeping on my info');
+// } else {
+//   alert('Try again!');
+//   // console.log('Try again!');
+// }
 
-var myPassion = prompt('Do I want to go into dental school now?');
-myPassion = myPassion.toLowerCase();
+// var myPassion = prompt('Do I want to go into dental school now?');
+// myPassion = myPassion.toLowerCase();
 
-// console.log(myPassion);
+// // console.log(myPassion);
 
-if (myPassion === 'yes' || myPassion === 'y') {
-  alert('Unfortunately, not anymore.');
-  // console.log('Unfortunately, not anymore.')
-} else {
-  alert('Yes that\'s correct! Let get that coding job!');
-  // console.log('Yes that\'s correct! Let get that coding job!')
-}
+// if (myPassion === 'yes' || myPassion === 'y') {
+//   alert('Unfortunately, not anymore.');
+//   // console.log('Unfortunately, not anymore.')
+// } else {
+//   alert('Yes that\'s correct! Let get that coding job!');
+//   // console.log('Yes that\'s correct! Let get that coding job!')
+// }
 
-var moveSeattle = prompt('Did I move to Seattle 2 years ago?');
-moveSeattle = moveSeattle.toLowerCase();
+// var moveSeattle = prompt('Did I move to Seattle 2 years ago?');
+// moveSeattle = moveSeattle.toLowerCase();
 
-// console.log(moveSeattle)
+// // console.log(moveSeattle)
 
-if (moveSeattle === 'yes' || moveSeattle === 'y') {
-  alert('Sorry, I am only 1 year old Seattlian.');
-  // console.log('Sorry, I am only 1 year old Seattlian.')
-} 
-else {
-  alert('Correct, I been enjoying here!');
-  // console.log('Correct, I been enjoying here!')
-}
+// if (moveSeattle === 'yes' || moveSeattle === 'y') {
+//   alert('Sorry, I am only 1 year old Seattlian.');
+//   // console.log('Sorry, I am only 1 year old Seattlian.')
+// } 
+// else {
+//   alert('Correct, I been enjoying here!');
+//   // console.log('Correct, I been enjoying here!')
+// }
 
-var myGoal = prompt('Do I want to build a app to take over the WORLD?');
-myGoal = myGoal.toLowerCase();
+// var myGoal = prompt('Do I want to build a app to take over the WORLD?');
+// myGoal = myGoal.toLowerCase();
 
-// console.log(myGoal);
+// // console.log(myGoal);
 
-if (myGoal === 'yes' || myGoal === 'y') {
-  alert('uh oh ' + askName + ', you know my secret. HAHA just kidding, I am not!?');
-  // console.log('uh oh ' + askName + ', you know my secret. HAHA just kidding, I am not!?')
-} else {
-  alert('Ding Ding Ding ' + askName + ', That\'s right I want to build an app to help underserved people.');
-  // console.log('Ding Ding Ding ' + askName + ', That\'s right I want to build an app to help underserved people.')
-}
+// if (myGoal === 'yes' || myGoal === 'y') {
+//   alert('uh oh ' + askName + ', you know my secret. HAHA just kidding, I am not!?');
+//   // console.log('uh oh ' + askName + ', you know my secret. HAHA just kidding, I am not!?')
+// } else {
+//   alert('Ding Ding Ding ' + askName + ', That\'s right I want to build an app to help underserved people.');
+//   // console.log('Ding Ding Ding ' + askName + ', That\'s right I want to build an app to help underserved people.')
+// }
+
+
+
+
+// for (var i = 0; i < 4;i++) {
+//   var countryVisit = prompt('How many countries have i visited?');
+
+//   // console.log('before parseint ' + typeof countryVisit)  
+  
+//   countryVisit = parseInt(countryVisit)
+
+//   // console.log('after paseint ' + typeof countryVisit)
+
+// if (countryVisit > 3) {
+//   alert('Sadly, I haven\'t been to that many countries yet.');
+// } else if (countryVisit < 3) {
+//   alert('I been fortunate enough to visit more than that');
+// } else if (countryVisit === 3 || countryVisit === 'three') {
+//   alert('Great job!');
+// i = 5
+// }
+
+// }
+
+
+
+
+
+// var favoriteFood = prompt ('What is my favorite food?');
+// favoriteFood = favoriteFood.toUpperCase();
+// var listFood = ['SUSHI', 'KIMCHI FRIED RICE', 'SUKIYAKI', 'HOT POT', 'YELLOW CURRY', 'PASTA', 'ITALIAN SUB SANDWICH', 'LOCO MOCO', 'HAMBURGER', 'PIZZA']
+
+// // while (favoriteFood) {
+//   for (var i=0;i < listFood.length; i++) {
+//     if (favoriteFood === listFood[i]) {
+//      alert('Hey you got it right buddy!');
+//      i = listfood.length + 1
+//     } else {
+//       favoriteFood = prompt('Try again for different food!');
+//     }
+    
+//     console.log(listFood[i])
+
+//   }
+
+
+  var favoriteFood = prompt ('What is my favorite food?');
+  favoriteFood = favoriteFood.toUpperCase();
+  var listFood = ['SUSHI', 'KIMCHI FRIED RICE', 'SUKIYAKI', 'HOT POT', 'YELLOW CURRY', 'PASTA', 'ITALIAN SUB SANDWICH', 'LOCO MOCO', 'HAMBURGER', 'PIZZA']
+  
+  // while (favoriteFood) {
+    for (var i=0;i < listFood.length; i++) {
+      if (favoriteFood === listFood[i]) {
+       alert('Hey you got it right buddy!');
+       i = listfood.length + 1
+      } else {
+        favoriteFood = prompt('Try again for different food!');
+      }
+      
+      console.log(listFood[i])
+  
+    }
+
+// }
+
+
+
+
+
+
+
 
 alert('Thank for taking your time to take my quizes ' + askName + ', I hope you learn little bit more about myself.');
